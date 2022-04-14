a8f4471940b0c33df1cceaad01f2197e8d49a05f
ly <314452354@qq.com>
2022-04-14 09:06:29 +0800

    帮助类文件

---
+++ ExpensesSys/ExpensesSys/Helper/ActualExpenseHelper.cs
@@ -0,0 +1,46 @@
+﻿using ExpensesSysLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesSysLib.Helper
+{
+    public class ActualExpenseHelper
+    {
+        public static async Task CalcActualExpenseAsync()
+        {
+            var service = new DBService();
+            var construct = "select ConstructID,sum(ActualExpense) as 'Total' from T_ConstructItem group by ConstructID";
+            var secondary = "select SecondaryConstructID,sum(ActualExpense) as 'Total' from T_Construct group by SecondaryConstructID";
+            var top = "select TopConstructID,sum(ActualExpense) as 'Total' from T_SecondaryConstructCategory group by TopConstructID";
+            var budget = "select BudgetID,sum(ActualExpense) as 'Total' from T_TopConstructCategory group by BudgetID";
+
+            var updateConstruct = "update T_Construct set ActualExpense=@ActualExpense,Surplus=Money-@Expense where ConstructID=@ID";
+            var updateSecondary = "update T_SecondaryConstructCategory set ActualExpense=@ActualExpense,Surplus=Money-@Expense where SecondaryConstructID=@ID";
+            var updateTop = "update T_TopConstructCategory set ActualExpense=@ActualExpense,Surplus=Money-@Expense where TopConstructID=@ID";
+            var updateBudget = "update T_Budget set ActualExpense=@ActualExpense,Surplus=Money-@Expense where BudgetID=@ID";
+
+            using (var c = new SQLiteConnection(DBHelper.GetSQLiteConnectString()))
+            {
+                var constructExpense = await service.QueryAsync<dynamic>(construct);
+                foreach (var r in constructExpense)
+                    await service.ExecuteAsync(updateConstruct, new { ActualExpense = r.Total, Expense = r.Total, ID = r.ConstructID });
+
+                var secondaryExpense = await service.QueryAsync<dynamic>(secondary);
+                foreach (var r in secondaryExpense)
+                    await service.ExecuteAsync(updateSecondary, new { ActualExpense = r.Total, Expense = r.Total, ID = r.SecondaryConstructID });
+
+                var topExpense = await service.QueryAsync<dynamic>(top);
+                foreach (var r in topExpense)
+                    await service.ExecuteAsync(updateTop, new { ActualExpense = r.Total, Expense = r.Total, ID = r.TopConstructID });
+
+                var budgetExpense = await service.QueryAsync<dynamic>(budget);
+                foreach (var r in budgetExpense)
+                    await service.ExecuteAsync(updateBudget, new { ActualExpense = r.Total, Expense = r.Total, ID = r.BudgetID });
+            }
+        }
+    }
+}
---
+++ ExpensesSys/ExpensesSys/Helper/DBHelper.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ExpensesSysLib.Helper
+{
+    public class DBHelper
+    {
+         private DBHelper()
+        {
+            
+        }
+        private static string path = Path.Combine(AppContext.BaseDirectory,"data.dll");
+
+        public static bool CheckDBExists()
+        {
+            return File.Exists(path);
+        }
+
+        public static string GetSQLiteConnectString()
+        {
+            var builder = new SQLiteConnectionStringBuilder
+            {
+                DataSource = path,
+                Version = 3,
+            };
+            return builder.ToString();
+        }
+
+        public static async void CreateTables()
+        {
+            if (CheckDBExists())
+                return;
+            var department = "create table T_Department(" +
+                "DepartmentID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null unique," +
+                "Remark nvarchar(200));";
+
+            var office = "create table T_AdministrativeOffice(" +
+                "OfficeID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null unique," +
+                "Remark nvarchar(200)," +
+                "DepartmentID nvarchar(150) not null references T_Department(DepartmentID));";
+
+            var budget = "create table T_Budget(" +
+                "BudgetID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null unique," +
+                "Money decimal not null," +
+                "ActualExpense decimal," +
+                "Surplus decimal," +
+                "Remark nvarchar(200)," +
+                "DepartmentID nvarchar(150) not null references T_Department(DepartmentID));";
+
+            var top = "create table T_TopConstructCategory(" +
+                "TopConstructID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null unique," +
+                "Money deciman not null," +
+                "ActualExpense decimal," +
+                "Surplus decimal," +
+                "Remark nvarchar(200)," +
+                "BudgetID nvarchar(150) not null references T_Budget(BudgetID));";
+
+            var secondary = "create table T_SecondaryConstructCategory(" +
+                "SecondaryConstructID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null unique," +
+                "Money deciman not null," +
+                "ActualExpense decimal," +
+                "Surplus decimal," +
+                "Remark nvarchar(200)," +
+                "TopConstructID nvarchar(150) not null references T_TopConstructCategory(TopConstructID));";
+
+            var construct = "create table T_Construct(" +
+                "ConstructID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null," +
+                "Unit nvarchar(50) not null," +
+                "QTY double," +
+                "Price decimal," +
+                "Money decimal," +
+                "Surplus decimal," +
+                "ActualExpense decimal," +
+                "Contractor nvarchar(5)," +
+                "ProcurementMode nvarchar(50)," +
+                "PlanCompletedDate date," +
+                "ActualCompletedDate date," +
+                "ApprovalMode nvarchar(50)," +
+                "Remark nvarchar(200)," +
+                "SecondaryConstructID nvarchar(150) not null references T_SecondaryConstructCategory(SecondaryConstructID)," +
+                "OfficeID nvarchar(150) not null references T_AdministrativeOffice(OfficeID));";
+
+            var item = "create table T_ConstructItem(" +
+                "ConstructItemID nvarchar(150) not null primary key," +
+                "Name nvarchar(50) not null," +
+                "Unit nvarchar(50) not null," +
+                "QTY double," +
+                "Price decimal," +
+                "Money decimal," +
+                "Surplus decimal," +
+                "ActualExpense decimal," +
+                "Contractor nvarchar(5)," +
+                "ProcurementMode nvarchar(50)," +
+                "PlanCompletedDate date," +
+                "ActualCompletedDate date," +
+                "ApprovalMode nvarchar(50)," +
+                "Remark nvarchar(200)," +
+                "ConstructID nvarchar(150) not null references T_Construct(ConstructID));";
+
+            using (var c = new SQLiteConnection(GetSQLiteConnectString()))
+            {
+                c.Open();
+                var department_command = new SQLiteCommand(department, c);
+                await department_command.ExecuteNonQueryAsync();
+                var office_command = new SQLiteCommand(office, c);
+                await office_command.ExecuteNonQueryAsync();
+                var budget_command = new SQLiteCommand(budget, c);
+                await budget_command.ExecuteNonQueryAsync();
+                var top_command = new SQLiteCommand(top, c);
+                await top_command.ExecuteNonQueryAsync();
+                var secondary_command = new SQLiteCommand(secondary, c);
+                await secondary_command.ExecuteNonQueryAsync();
+                var construct_command = new SQLiteCommand(construct, c);
+                await construct_command.ExecuteNonQueryAsync();
+                var item_command = new SQLiteCommand(item, c);
+                await item_command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
