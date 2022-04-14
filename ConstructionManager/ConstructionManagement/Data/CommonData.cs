89974490f042a5b2ce82538e3b1e810913337668
ly <314452354@qq.com>
2022-04-14 08:51:11 +0800

    辅助服务

---
+++ ConstructionManager/ConstructionManagement/Data/CommonData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionManagement.Data
+{
+    public class CommonData
+    {
+        public static List<string> ConstructionPhase = new List<string>
+        {
+            "开工日期",
+            "土建施工",
+            "电气施工",
+            "电子化移交",
+            "停电时间1",
+            "停电时间2",
+            "停电时间3",
+            "投运时间",
+            "竣工时间",
+            "结算时间",
+            "送结时间",
+            "归档",
+            "其他"
+        };
+    }
+}
---
+++ ConstructionManager/ConstructionManagement/Data/Helper.cs
@@ -0,0 +1,23 @@
+﻿using ConstructionManagement.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionManagement
+{
+    public class Helper
+    {
+        public static SortableBindingList<T> GetSortableBindingList<T>(IList<T> datas) where T:class
+        {
+            var result = new SortableBindingList<T>();
+            foreach(var r in datas)
+            {
+                result.Add(r);
+            }
+            return result;
+        }
+    }
+}
