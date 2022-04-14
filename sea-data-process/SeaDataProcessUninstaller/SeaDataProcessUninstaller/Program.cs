0c01da6dd7d3117866c5a997ee595f6452d62454
ly <314452354@qq.com>
2022-04-14 08:33:07 +0800

    波浪数据处理软件卸载项目

---
+++ sea-data-process/SeaDataProcessUninstaller/SeaDataProcessUninstaller/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaDataProcessUninstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string root = System.Environment.SystemDirectory;
+            string uninstallPath = root + @"\msiexec.exe ";
+            System.Diagnostics.Process.Start(uninstallPath, "/x {5AE0EE84-490D-4380-BB2B-0CEE61B6AA6B}");
+        }
+    }
+}
